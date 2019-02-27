0800fe4f997cb627e4e3583787926230ad823be3
whimmy <whimmy@126.com>
2019-02-27 16:04:12 +0800

    format code

--- kotlin-junit5/src/test/kotlin/com/ymmihw/kotlin/junit5/CalculatorTest5.kt
+++ kotlin-junit5/src/test/kotlin/com/ymmihw/kotlin/junit5/CalculatorTest5.kt
@@ -58,12 +58,11 @@ class CalculatorTest5 {
 		3 to 9,
 		4 to 16,
 		5 to 25
-	)
-		.map { (input, expected) ->
-			DynamicTest.dynamicTest("when I calculate $input^2 then I get $expected") {
-				Assertions.assertEquals(expected, calculator.square(input))
-			}
+	).map { (input, expected) ->
+		DynamicTest.dynamicTest("when I calculate $input^2 then I get $expected") {
+			Assertions.assertEquals(expected, calculator.square(input))
 		}
+	}
 
 	private val squaresTestData = listOf(
 		1 to 1,
